331bef6af6e6a5a1ca9ea4befdcdbb8c1e07f218
Andrew Croce <andrew.croce@gmail.com>
2018-08-13 10:46:22 -0400

    fetch prices from api

---
+++ src/api.js
@@ -0,0 +1,36 @@
+const api = 'https://suitor-front-end-back-end.herokuapp.com'
+
+// You'll need an API key to access the API.
+// To get one, visit: https://suitor-front-end-back-end.herokuapp.com/api_key
+
+const apiKey = ''
+
+const pricingEndpoints = {
+  vcpu: '/prices/vcpu',
+  ram: '/prices/ram',
+  disk: '/prices/disk',
+  os: '/prices/os'
+}
+
+export const fetchPrices = () => {
+  console.log('Lets fetch some VM pricing!')
+
+  let prices = {}
+
+  return Promise.all(Object.keys(pricingEndpoints).map(key => {
+    console.log('   fetching ', pricingEndpoints[key], '...')
+
+    return fetch(api + pricingEndpoints[key], {
+      method: 'GET',
+      headers: { 'API-Key': apiKey }
+    })
+    .then(response => response.json())
+    .then(json => { prices[key] = json })
+    .catch(error => console.error(error))
+  }))
+  .then(() => {
+    console.log('Prices fetched! ', prices )
+    return prices
+  })
+  .catch(error => console.error(error))
+}
